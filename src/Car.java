90d90821fbcbbebf93fae2459ec8392cc9a8f68c
PolinaRrr <poniapocalypsisa@gmail.com>
2020-10-07 13:15:53 +0300

    Commit#2 normal code

--- src/Car.java
+++ src/Car.java
@@ -1,34 +1,62 @@
-public class Car implements Runnable{
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static boolean isWinner;
+
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
+    private CyclicBarrier cyclicBarrier;
+    private CountDownLatch countDownLatch;
+
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier, CountDownLatch countDownLatch) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
+        this.countDownLatch = countDownLatch;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            cyclicBarrier.await();
+            cyclicBarrier.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            checkWin(this);
+            cyclicBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+
+    }
+
+    private static synchronized void checkWin(Car c) {
+        if (!isWinner) {
+            System.out.println(c.name + " ПОБЕДИЛ");
+            isWinner = true;
         }
     }
+
 }
